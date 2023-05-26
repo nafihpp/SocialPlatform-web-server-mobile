cef175afb5f0756339c555b226ad691fc28561c3
nafihpp <nafihpp@gmail.com>
2023-05-27 02:26:34 +0530

    first commit

---
+++ src/pages/InstagramFeed.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import { SideNavbar } from "../components/SideNavbar";
+import { StoriesBar } from "../components/StoriesBar";
+import { SuggestionBar } from "../components/SuggestionBar";
+
+export const InstagramFeed = () => {
+    return (
+        <Fragment>
+            <div className="flex">
+                <SideNavbar />
+                <StoriesBar />
+                <SuggestionBar />
+            </div>
+        </Fragment>
+    );
+};

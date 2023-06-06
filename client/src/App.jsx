c66f993c22e693c7f7d96f77b9fac234343621d5
nafihpp <nafihpp@gmail.com>
2023-06-06 13:40:09 +0530

    bug: nprogress fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
     useEffect(() => {
         nProgress.start();
-        nprogress.done();
+        nProgress.done();
     }, [location.pathname]);
     return (
         <Routes>

88f5f5764888f6f2d8d334b30a47490cfd3af32c
MOHAMMED NAFIH YUSEPH <49452140+nafihpp@users.noreply.github.com>
2023-06-06 13:21:40 +0530

    Update StoriesBar.jsx

--- client/src/components/StoriesBar/StoriesBar.jsx
+++ client/src/components/StoriesBar/StoriesBar.jsx
@@ -1,5 +1,126 @@
 import React from "react";
 
 export const StoriesBar = () => {
-    return <div>StoriesBar</div>;
+    return ( <div className="bg-red-900 w-[800px] h-28 flex justify-between items-center px-4 mt-2 overflow-x-auto">
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden ">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden ">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden ">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[1px] border-[red] overflow-hidden">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className="h-[100%] w-[100%]"
+                />
+            </div>
+        </div>)
 };

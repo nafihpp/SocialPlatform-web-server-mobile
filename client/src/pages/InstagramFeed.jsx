aed9b7c377404f07ffbeff821594b9cf4a9c6f44
nafihpp <nafihpp@gmail.com>
2023-06-06 13:50:32 +0530

    bug: stories bug fix

--- client/src/pages/InstagramFeed.jsx
+++ client/src/pages/InstagramFeed.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import { SideNavbar } from "../components/SideNavbar";
 import { InstaPosts } from "../components/InstaPosts";
+import { StoriesBar } from "../components/StoriesBar";
 
 export const InstagramFeed = () => {
     return (
         <div className="flex max-h-screen w-[100%]">
             <SideNavbar />
             <div className="w-full">
+                <StoriesBar />
                 <InstaPosts />
             </div>
         </div>

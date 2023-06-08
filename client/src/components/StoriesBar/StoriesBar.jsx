f60d28229d986e085d0e0dd2d5b8f029f66b0815
nafihpp <nafihpp@gmail.com>
2023-06-09 03:21:54 +0530

    intial: folder structure

--- client/src/components/StoriesBar/StoriesBar.jsx
+++ client/src/components/StoriesBar/StoriesBar.jsx
@@ -3,100 +3,123 @@ import React from "react";
 export const StoriesBar = () => {
     return (
         <div className="bg-[#f9f9f9] w-full h-28 flex justify-between items-center px-4 mt-2 overflow-x-auto">
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="rounded-[50%] flex-shrink-0 w-[100px] h-[70px] mr-4 border-[3px] border-[red] overflow-hidden ">
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
                 <img
                     src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
-                    className="h-[100%] w-[100%]"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
+                />
+            </div>
+            <div className="rounded-[50%] flex-shrink-0 mr-4 border-[3px] border-[red] overflow-hidden w-20 h-20 ">
+                <img
+                    src="https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+                    className=" w-20 h-20"
+                    style={{ objectFit: "cover" }}
                 />
             </div>
         </div>

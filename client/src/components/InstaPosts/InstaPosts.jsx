8db5407ccde310687e7ee1ddae27624a509a025c
nafihpp <nafihpp@gmail.com>
2023-06-20 15:33:42 +0530

    bug-fix

--- client/src/components/InstaPosts/InstaPosts.jsx
+++ client/src/components/InstaPosts/InstaPosts.jsx
@@ -3,7 +3,7 @@ import { PostCard } from "../PostCard";
 
 export const InstaPosts = () => {
     return (
-        <div className="overflow-scroll h-[100vh - 162px] flex flex-col items-center">
+        <div className="overflow-scroll h-[100vh - 162px] flex flex-col items-center w-[90%] mx-auto">
             <PostCard />
             <PostCard />
             <PostCard />

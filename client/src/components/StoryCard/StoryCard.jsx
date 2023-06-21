2698773222766674d3d7f48b5fa8ff584a787963
nafihpp <nafihpp@gmail.com>
2023-06-21 23:51:01 +0530

    bug-fix

--- client/src/components/StoryCard/StoryCard.jsx
+++ client/src/components/StoryCard/StoryCard.jsx
@@ -1,6 +1,6 @@
 export const StoryCard = ({ image }) => {
     return (
-        <div className="flex-shrink-0 ml-2 w-[100px] h-[100px]">
+        <div className="flex-shrink-0 ml-2 w-[100px]">
             <div className="bg-gradient-to-tr from-yellow-500 to-fuchsia-600 rounded-full cursor-pointer w-[85px] h-[85px] flex items-center justify-center">
                 <a className="">
                     <img
@@ -10,7 +10,7 @@ export const StoryCard = ({ image }) => {
                     />
                 </a>
             </div>
-            <p className="w-[100%] truncate text-center">nafih888888</p>
+            <p className="w-[80%] truncate text-center">nafih888888</p>
         </div>
     );
 };
